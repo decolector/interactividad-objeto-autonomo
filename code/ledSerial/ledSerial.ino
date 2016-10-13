24a024e4fa2e89304cd73566639653033f9a64d0
Camilo Martinez <cmart@decolector.net>
2016-10-13 16:58:26 -0500

    bugfix

--- code/ledSerial/ledSerial.ino
+++ code/ledSerial/ledSerial.ino
@@ -27,9 +27,8 @@
 
  */
 
-int sensorPin = A0;    // select the input pin for the potentiometer
 int outPin = 3;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
+int val = 0;  // variable to store the value coming from the sensor
 
 void setup() {
   // declare the ledPin as an OUTPUT:
